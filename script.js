3886d6006aa709f640da076fbe0a40a4d43da3db
shahriar nafis <snafaro02@gmail.com>
2025-09-06 18:20:44 +0600

    nav section animation created

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("animated wizardz website")
+const timeLine = gsap.timeline();
+
+timeLine.from("nav h1",{
+    y: -50,
+    opacity: 0,
+    duration: .8
+})
+
+timeLine.from(".links h4",{
+    y:-50,
+    opacity:0,
+    duration:.8,
+    stagger:.2
+})
+
+timeLine.from(".links button",{
+    y:-50,
+    opacity:0,
+    duration:.5
+},"-=.2")
